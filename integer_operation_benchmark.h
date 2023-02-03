2ce3f9793afe8d435920861c3d62cfa5ae76f052
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2023-02-03 03:37:03 -0500

    calculated time (in seconds)
    Calculated time (in seconds) by subtracting the start clock time from the end clock time and dividing the result by CLOCKS_PER_SEC.
    
    Also, print the result.

--- integer_operation_benchmark.h
+++ integer_operation_benchmark.h
@@ -11,4 +11,10 @@ int main(){
         // 10^10 additions (of integer constants)
         int_sum = int1 + int2;
     }
+    end = clock();
+    printf("32-bit Integer Operation Benchmark");
+    printf("Reference Time: 100 seconds");
+    printf("Time Taken: %lf seconds",(double)(end - start) / CLOCKS_PER_SEC);
+
+    return 0;
 }
