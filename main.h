f6678a26cbca88f365a1b67ee8851301a00c0e80
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2023-02-03 01:37:09 -0500

    created c files
    created the main file and benchmark files, and included the benchmark files in the main file.

---
+++ main.h
@@ -0,0 +1,5 @@
+#include <integer_operation_benchmark.h>
+#include <floating_point_operation_benchmark.h>
+#include <memory_benchmark.h>
+#include <hard_drive_benchmark1.h>
+#include <hard_drive_benchmark2.h>
