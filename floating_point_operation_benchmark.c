5892c6daac6020c211a5222eae62ae5d5f6176b6
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2023-02-06 11:18:51 -0500

    declared double variables, clock_t instances
    declared the double precision variables for x, y and the result of the operations. Also, started and stopped the clock.

--- floating_point_operation_benchmark.c
+++ floating_point_operation_benchmark.c
@@ -2,5 +2,12 @@
 #include <time.h>
 
 int main(){
+  double x = 225452315454.54654682, y = 2354356785.534658748, result;
+  clock_t start, end;
+  
+  start = clock();
+  
+  end = clock();
+  
   return 0;
 }
