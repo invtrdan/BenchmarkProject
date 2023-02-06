85ec77bb2db4364f8162dc38cfc50a7697f07187
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2023-02-06 11:24:47 -0500

    Update floating_point_operation_benchmark.c

--- floating_point_operation_benchmark.c
+++ floating_point_operation_benchmark.c
@@ -13,5 +13,8 @@ int main(){
   
   end = clock();
   
+  printf("Reference Time: 100 seconds")
+  printf("Time taken for 1000000000 floating point multiplications: %.2lf seconds\n",((double) (end - start)) / CLOCKS_PER_SEC);
+  
   return 0;
 }
