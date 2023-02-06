d4037e79e5b3d64fe3271e046e3e6f1a1e8f4294
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2023-02-06 11:27:49 -0500

    formatting
    Added missing semicolon online 17

--- floating_point_operation_benchmark.c
+++ floating_point_operation_benchmark.c
@@ -14,7 +14,7 @@ int main(){
   end = clock();
   
   printf("Reference Time: 100 seconds");
-  printf("Time taken for 1000000000 floating point multiplications: %.2lf seconds\n",((double) (end - start)) / CLOCKS_PER_SEC);
+  printf("\nTime taken for 1000000000 floating point multiplications: %.2lf seconds\n",((double) (end - start)) / CLOCKS_PER_SEC);
   
   return 0;
 }
