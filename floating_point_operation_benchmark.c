17cd46d8dd36db07e01b874231c3956aead57c53
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2023-02-06 11:25:41 -0500

    corrected error (;)
    added the missing semicolon on line16.

--- floating_point_operation_benchmark.c
+++ floating_point_operation_benchmark.c
@@ -13,7 +13,7 @@ int main(){
   
   end = clock();
   
-  printf("Reference Time: 100 seconds")
+  printf("Reference Time: 100 seconds");
   printf("Time taken for 1000000000 floating point multiplications: %.2lf seconds\n",((double) (end - start)) / CLOCKS_PER_SEC);
   
   return 0;
