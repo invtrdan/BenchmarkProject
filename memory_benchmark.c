717fa466b11ec1411b3d05bd3c59f412fc8e2384
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2023-02-07 16:34:26 -0500

    error message for malloc
    print an error message if there is an issue allocating memory

--- memory_benchmark.c
+++ memory_benchmark.c
@@ -9,6 +9,10 @@ int main(){
   int i;
   clock_t start, end;
   int *data = malloc(NUM_ELEMENTS * BYTES_PER_ELEMENT);
+  if (data == NULL) {
+    printf("Error allocating memory!\n");
+    return 1;
+  }
   
   return 0;
 }
