18581df9db443c1d2ae534be48ada4490075cf4a
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2023-02-07 16:33:16 -0500

    define variables
    define BYTES_PER_ELEMENT, clock objects (start, stop) and allocate memory for the data elements.

--- memory_benchmark.c
+++ memory_benchmark.c
@@ -3,7 +3,12 @@
 #include <time.h>
 
 #define SIZE 500000
+#define BYTES_PER_ELEMENT 4
 
 int main(){
+  int i;
+  clock_t start, end;
+  int *data = malloc(NUM_ELEMENTS * BYTES_PER_ELEMENT);
+  
   return 0;
 }
